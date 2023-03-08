52b6eb61776bb49b64cacaababc5da6a807f066d
Manaa Ahmed <ahmed.manaa@esprit.tn>
2023-03-08 13:17:21 +0100

    twoFactorAuth

--- OASIS-back/server/models/user.js
+++ OASIS-back/server/models/user.js
@@ -25,6 +25,10 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    phonenumber: {
+      type: Number,
+      required: true,
+    },
     email: {
       type: String,
       required: true,
