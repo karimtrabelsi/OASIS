7c3a093e8986f6adf1fbc8cad3e4616c64c3d0f9
karimtrabelsi <karimtrabelsi1@esprit.tn>
2023-03-10 21:29:48 +0100

    added table filters/still not perfect

--- OASIS-front/src/jsx/components/table/BootstrapTable.js
+++ OASIS-front/src/jsx/components/table/BootstrapTable.js
@@ -34,6 +34,9 @@ const BootstrapTable = () => {
   );
 
   const [users, setUsers] = useState([]);
+  const [bannedUsers, setBannedUsers] = useState(false);
+  const [newUsers, setNewUsers] = useState(false);
+  const [search, setSearch] = useState("");
   useEffect(() => {
     axios
       .get("http://localhost:3000/users")
@@ -43,6 +46,10 @@ const BootstrapTable = () => {
       .catch((err) => {
         console.log(err);
       });
+
+    bannedUsers && setUsers(users.filter((user) => user.banned === true));
+    newUsers && setUsers(users.filter((user) => user.approved === false));
+    search && setUsers(users.filter((user) => user.username.includes(search)));
   }, [users]);
 
   const handleApprove = (id) => {
@@ -71,6 +78,51 @@ const BootstrapTable = () => {
               <Card.Title>Users</Card.Title>
             </Card.Header>
             <Card.Body>
+              <div className="">
+                <Badge
+                  variant={
+                    bannedUsers ? "dark badge-xl dark" : "light badge-xl light"
+                  }
+                  className="col-lg-2"
+                  onClick={() => setBannedUsers(!bannedUsers)}
+                >
+                  Banned Users{" "}
+                </Badge>
+
+                <Badge
+                  variant={
+                    newUsers ? "dark badge-xl dark" : "light badge-xl light"
+                  }
+                  className="col-lg-2"
+                  onClick={() => setNewUsers(!newUsers)}
+                >
+                  New Users{" "}
+                </Badge>
+                <div className="input-group search-area d-lg-inline-flex d-none mr-5">
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Search here"
+                    onChange={(e) => setSearch(e.target.value)}
+                  />
+                  <div className="input-group-append">
+                    <span className="input-group-text">
+                      <svg
+                        width={20}
+                        height={20}
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M23.7871 22.7761L17.9548 16.9437C19.5193 15.145 20.4665 12.7982 20.4665 10.2333C20.4665 4.58714 15.8741 0 10.2333 0C4.58714 0 0 4.59246 0 10.2333C0 15.8741 4.59246 20.4665 10.2333 20.4665C12.7982 20.4665 15.145 19.5193 16.9437 17.9548L22.7761 23.7871C22.9144 23.9255 23.1007 24 23.2816 24C23.4625 24 23.6488 23.9308 23.7871 23.7871C24.0639 23.5104 24.0639 23.0528 23.7871 22.7761ZM1.43149 10.2333C1.43149 5.38004 5.38004 1.43681 10.2279 1.43681C15.0812 1.43681 19.0244 5.38537 19.0244 10.2333C19.0244 15.0812 15.0812 19.035 10.2279 19.035C5.38004 19.035 1.43149 15.0865 1.43149 10.2333Z"
+                          fill="#A4A4A4"
+                        />
+                      </svg>
+                    </span>
+                  </div>
+                </div>
+              </div>
               <Table responsive>
                 <thead>
                   <tr>
@@ -162,27 +214,41 @@ const BootstrapTable = () => {
                               onClick={() =>
                                 swal({
                                   title: "Are you sure?",
-                                  text: "Once Banned, user will not be able to login !",
+                                  text: user.banned
+                                    ? "Unban User"
+                                    : "Once Banned, user will not be able to login !",
                                   icon: "warning",
                                   buttons: true,
                                   dangerMode: true,
-                                }).then(() =>
-                                  axios
-                                    .post(
-                                      "http://localhost:3000/users/ban/" +
-                                        user._id
-                                    )
-                                    .then((res) => {
-                                      // console.log(res)
-                                      if ((res.respone = 200)) {
-                                        swal("User Banned !", {
-                                          icon: "success",
-                                        });
-                                      } else {
-                                        swal("Nothing changed !");
-                                      }
-                                    })
-                                )
+                                }).then((willBan) => {
+                                  if (willBan) {
+                                    axios
+                                      .post(
+                                        "http://localhost:3000/users/ban/" +
+                                          user._id
+                                      )
+                                      .then((res) => {
+                                        // console.log(res)
+                                        if ((res.respone = 200)) {
+                                          swal(
+                                            user.banned
+                                              ? "User Unbanned!"
+                                              : "User Banned !",
+                                            {
+                                              icon: "success",
+                                            }
+                                          );
+                                        } else {
+                                          swal("Connection Error!");
+                                        }
+                                      })
+                                      .catch((err) => {
+                                        console.log(err);
+                                      });
+                                  } else {
+                                    swal("Nothing changed !");
+                                  }
+                                })
                               }
                             >
                               {user.banned ? "Unban" : "Ban"}
